fa66dadcf892ef12d6b9d180a78cb08978a77dae
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-29 14:17:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require('./scr/schema')
+const resolvers = require('./scr/resolvers')
+const startDatabase = require('./scr/database');
+const expressPlayground = require("graphql-playground-middleware-express").default;
+
+// Create a context for holding contextual data 
+const context = async () => {
+  const db = await startDatabase();
+
+  return { db };
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    context
+  })
+);
+
+//Graphql Playground route
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+const port = process.env.PORT || "4000";
+
+app.listen(port);
+
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
